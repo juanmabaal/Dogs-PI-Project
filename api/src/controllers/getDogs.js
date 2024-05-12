0f05cadc289cbedd79654633bd20cf177286a687
Juanma <juanma.baal@gmail.com>
2024-05-11 19:56:17 -0500

    Se crea el controlador getDogs para traer los datos desde la api y la base de datos, finalmente se concatenan

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -3,12 +3,46 @@ const axios = require('axios');
 
 const { API_KEY } = process.env;
 const URL = `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`;
+const { Dog, Temperament } = require('../db');
 
 const getDogs = async () => {
 
     try {
         const { data } = await axios(URL);
-        console.log("Datos recibidos de The Dog API:", data);
+
+        const apiBreeds = data?.map((dog) => {
+            return {
+                id: dog?.id,
+                name: dog?.name,
+                weight: dog?.weight?.metric,
+                height: dog?.height?.metric,
+                life_span: dog?.life_span,
+                temperament: dog?.temperament,
+                image: dog?.image?.url,
+                created: false,
+            }
+        });
+
+        const DB_breeds = await Dog.findAll ({
+            include: {
+                model: Temperament}})
+        
+        const DB_dog = DB_breeds.map((dog) => {
+            const breedTempDB = Dog.Temperament.map(temp => temp.name);
+            return {
+                id: dog.id,
+                name: dog.name,
+                weight: dog.weight,
+                height: dog.height,
+                life_span: dog.life_span,
+                temperament: dog.tetmperamen,
+                image: dog.image,
+                created: true,
+                temperament: breedTempDB.join(", ")
+            }
+        })
+        
+        return[...DB_dog, ...apiBreeds];
     } catch (error) {
         console.error("Error al obtener datos de The Dog API:", error);
     }

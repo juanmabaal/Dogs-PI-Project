2c8dcc22f99b713b6ddec3ef450d04d924fae0d8
Juanma <juanma.baal@gmail.com>
2024-05-20 03:40:40 -0500

    Se hacen algunas modificaciones al controller getTemperamentDogApi para que cree los temperamentos en la base de datos

--- api/src/controllers/getTemperamentDogApi.js
+++ api/src/controllers/getTemperamentDogApi.js
@@ -12,28 +12,25 @@ const getTemperamentDogApi = async() => {
 
         if (!data) throw new Error("No se recibieron datos desde la API.");
 
-        const temperamentDogApi = data?.map((dog) => dog?.temperament);
+        const temperamentDogApi = data.map((dog) => dog.temperament).filter(Boolean); //Filtrar Temperamentos Vacíos: Utilizamos .filter(Boolean) para eliminar entradas undefined o null
         
         let allTemps = temperamentDogApi.toString().split(",");
         
-        allTemps = allTemps.filter((temp) => temp !== ""); //Se obtienen los temperamentos individuales, eliminando las cadenas vacias ""
+        allTemps = allTemps.map(temp => temp.trim()).filter(temp => temp !== ""); //Se obtienen los temperamentos individuales, eliminando las cadenas vacias "", trim para que elimine los espacios en blanco de los temperamentos
 
-        const tempsFiltered = allTemps.filter((temp, index) => {
-            return allTemps.indexOf(temp) === index;
-        }) //Si el índice de un temperamento es igual al índice de su primera aparición en el array, significa que es único y se conserva en el array tempsFiltered
+
+        const tempsFiltered = [...new Set(allTemps)]; // Filtrar únicos utilizando Set
 
         if (tempsFiltered.length === 0) throw new Error("No se encontraron temperamentos válidos.");
-
+        
 
         const temperamentObject = tempsFiltered.map((temp) => { //Se convierten los temperamentos en un objeto y luego pasarlos al modelo
             return {
                 name: temp
             };
         });
+        await Temperament.bulkCreate(temperamentObject, { ignoreDuplicates: true }); //Evitamos duplicados
 
-        temperamentObject.forEach(temp => {
-            if(temp) Temperament.bulkCreate({ name: temp })
-        });
 
         return temperamentObject;
 

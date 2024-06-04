879ffb369725af68d122b3ea90306f12530ea3e0
Juanma <juanma.baal@gmail.com>
2024-06-03 21:38:34 -0500

    Se crea el test para el modelo de perros

--- api/tests/models/dog.spec.js
+++ api/tests/models/dog.spec.js
@@ -6,17 +6,50 @@ describe('Dog model', () => {
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
     }));
-  describe('Validators', () => {
-    beforeEach(() => Dog.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Dog.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
-      });
-      it('should work when its a valid name', () => {
-        Dog.create({ name: 'Pug' });
-      });
+
+    beforeEach( async () => {
+      await conn.sync({ force: true })
     });
-  });
-});
+
+    describe('Validations', () => {
+      it('should not create a Dog if name is null', async () => {
+        try {
+          await Dog.create({
+            weight: '20-25',
+            height: '30-40',
+            life_span: '10-12 years',
+            image: 'https://example.com/bulldog.jpg'
+          });
+        } catch (error) {
+          expect(error.message).to.contain('name cannot be null');
+        }
+      });
+
+      it('should create a Dog if all required fields are provides', async () => {
+          const dog = await Dog.create({
+            name: 'Bulldog',
+            weight: '20-25',
+            height: '30-40',
+            life_span: '10-12 years',
+            image: 'https://example.com/bulldog.jpg'
+          });
+          expect(dog.toJSON()).to.have.property('name', 'Bulldog');
+        });
+
+        it('should not create a Dog if weight is null', async () => {
+          try {
+            await Dog.create({
+              name: 'Bulldog',
+              height: '30-40',
+              life_span: '10-12 years',
+              image: 'https://example.com/bulldog.jpg'
+            });
+          } catch (error) {
+            expect(error.message).to.contain('weight cannot be null');
+          }
+        })
+      })
+
+    })
+  
+      
